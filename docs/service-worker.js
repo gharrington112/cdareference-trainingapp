8ee8dc11e0f4ef519b4e6fa8cdde1a373556c952
GitRerumu <achoran03@gmail.com>
2025-04-02 19:39:43 -0400

    deleted:    docs/favicon.ico 	modified:   docs/index.html 	new file:   docs/service-worker.js

---
+++ docs/service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/static/js/bundle.js",
+  "/static/js/main.chunk.js",
+  "/static/js/0.chunk.js",
+  "/static/css/main.chunk.css",
+  "/manifest.json",
+  "./assets/images/centurydentallogo2.png",
+];
+
+// Install the Service Worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch files from the cache when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate and clean up old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
