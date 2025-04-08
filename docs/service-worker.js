df02bc9b854f152c3de3cb18718a2a89d53d9d72
GitRerumu <achoran03@gmail.com>
2025-04-08 01:38:46 -0400

    Added manifest.json for future use. 	new file:   docs/manifest.json 	modified:   docs/service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = "pwa-cache-v4";
 const urlsToCache = [
   "./",
   "./index.html",
-  "./assets/images/*",
+  "./assets/assets/images/*",
   "./favicon.ico",
-  "/app.json",
+  "./metadata.json",
 ];
 
 // Install event: Cache core assets
