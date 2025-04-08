96916acf1e4de22dac53c294eac0ba7e7335db72
GitRerumu <achoran03@gmail.com>
2025-04-08 01:10:48 -0400

    Attempt 1 to fix service worker. 	modified:   docs/service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -2,9 +2,9 @@ const CACHE_NAME = "pwa-cache-v3";
 const urlsToCache = [
   "./",
   "./index.html",
-  "gitrerumu.github.io/assets/images/",
+  "gitrerumu.github.io/centurydental_test/assets/images/",
   "./favicon.ico",
-  "./manifest.json"
+  "./manifest.json",
 ];
 
 // Install event: Cache core assets
