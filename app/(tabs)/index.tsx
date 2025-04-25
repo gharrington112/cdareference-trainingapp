0e997e88300c8028ded03e05069b14d3ed32f077
Gregory Harrington <114744068+gharrington112@users.noreply.github.com>
2025-04-25 15:58:53 -0400

    Update index.tsx
    changing color scheme for light & dark mode

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ import { ThemedView } from '@/components/ThemedView';
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }} // need to change color scheme to make image viewable
+      headerBackgroundColor={{ light: '#EEEEEE', dark: '#222222' }} // need to change color scheme to make image viewable
 
       headerImage={
         <Image
